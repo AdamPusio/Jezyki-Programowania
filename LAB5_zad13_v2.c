d74efd7783d01483afbcef28d81c12082d79f5cb
mpusio <32813834+mpusio@users.noreply.github.com>
2017-11-30 19:16:11 +0100

    Praca domowa Lab 5 z "gcc -Wall -ansi program.c "

---
+++ LAB5_zad13_v2.c
@@ -0,0 +1,28 @@
+/*wercja z gcc -Wall -ascii*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n,m;
+  printf("Podaj dwie liczby do tablicy\n");
+  scanf("%d", &n);
+  scanf("%d", &m);
+  int macierz [4][4];
+  int i = 0;
+  int j = 0;
+
+  while (i < 4) {
+    while (j < 4) {
+      macierz [i][j] = i;
+      macierz [0][0] = n;
+      macierz [0][1] = m;
+      printf("%d ", macierz[i][j]);
+      j++;
+    }
+    printf("\n");
+    i++;
+    j=0;
+  }
+  return 0;
+}
